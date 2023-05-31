c48f331e25e60d168e7de545dbd619ab62965695
Travis Lawrence <travis.lawrence12@gmail.com>
2023-05-30 18:10:32 -0700

    options data

---
+++ players options/code.js
@@ -0,0 +1,17 @@
+// code to convert the array of json options (...Options.json) into the object file (...OptionsData.json)
+function reducePlayers(players) {
+  return players.reduce((acc, player) => {
+    if (!acc[player.baseballPlayerLocalID]) {
+      acc[player.baseballPlayerLocalID] = {};
+    }
+    acc[player.baseballPlayerLocalID][player.optionKey] = {
+      value: player.optionValue,
+      type: player.optionType,
+    };
+    return acc;
+  }, {});
+}
+
+// const fs = require('fs')
+// const smbOptionData = require("./SuperMegaOptions.json")
+// > fs.writeFile('./smbOptionsData.json', JSON.stringify(reducePlayers(smbOptionData)), err => {err})
