f3157c90c2e38fd4f7dec72b159f86fed35c2f34
Travis Lawrence <travis.lawrence12@gmail.com>
2023-06-08 20:38:09 -0700

    db seeding works

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,59 +1,28 @@
 const express = require("express");
 const { db } = require("../db");
 const router = express.Router();
+const playerJson = require("../seedData/playersComplete.json");
 
-const seedPlayer = async (player) => {
-  const user = await db.player.create({
-    data: player,
+const seedPlayer = async () => {
+  // make all attributes strings (probably should just migrate db and change this)
+  const formatted = playerJson.map((player) => {
+    return Object.keys(player).reduce((acc, key) => {
+      acc[key] = String(player[key]);
+      return acc;
+    }, {});
   });
-  console.log(user);
+  const count = await db.player.createMany({
+    data: formatted,
+  });
+  return count;
 };
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  // ok this worked! make sure the types are lined up
-  //  seedPlayer({
-  //   localID: "1",
-  //   teamName: "Sawteeth",
-  //   firstName: "Thrash",
-  //   lastName: "Allmeyer",
-  //   primaryPosition: "1",
-  //   pitcherRole: "1",
-  //   power: "2",
-  //   contact: "21",
-  //   speed: "24",
-  //   fielding: "82",
-  //   arm: "null",
-  //   velocity: "44",
-  //   junk: "56",
-  //   accuracy: "76",
-  //   age: "31",
-  //   trait: "31",
-  //   subType: "7",
-  //   league: "superMega",
-  //   gender: "",
-  //   throws: "1",
-  //   bats: "1",
-  //   secondaryPosition: "",
-  //   rating: "83.73633999999998",
-  //   trait1: "Wild Thing",
-  //   chemistry1: "Spirited",
-  //   trait2: "",
-  //   chemistry2: "",
-  //   careerStart: "",
-  //   careerEnd: "",
-  //   fourSeamFastball: "1",
-  //   twoSeamFastball: "",
-  //   screwball: "",
-  //   changeup: "1",
-  //   forkball: "",
-  //   curveball: "1",
-  //   slider: "1",
-  //   cutFastball: "",
-  //   windup: "2",
-  //   pitchAngle: "3",
-  // });
+router.get("/seed", async function (req, res, next) {
+  const playerCount = await seedPlayer();
+  res.json({ title: "All seeded!", playerCount });
+});
 
+router.get("/", function (req, res, next) {
   res.json({ title: "HEY" });
 });
 
