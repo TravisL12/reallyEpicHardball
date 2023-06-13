9f00f2ef18d50951bc90495972ddd8db3394ddc1
Travis Lawrence <travis.lawrence12@gmail.com>
2023-06-13 07:31:49 -0700

    its working

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,9 +2,18 @@ const express = require("express");
 const { db } = require("../db");
 const router = express.Router();
 
-router.get("/", async function (req, res, next) {
-  const teams = await db.player.findMany();
-  res.json({ teams });
+router.get("/team", async function (req, res, next) {
+  const { teamId } = req.query;
+  const team = await db.team.findUnique({ where: { id: +teamId } });
+  team.players = await db.player.findMany({
+    where: { teamId: String(teamId) },
+  });
+  res.json({ team });
+});
+
+router.get("/players", async function (req, res, next) {
+  const players = await db.player.findMany();
+  res.json({ players });
 });
 
 module.exports = router;
