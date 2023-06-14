6793df5fdfcbec1e5a89832fdaeffaa406c5bbf5
Travis Lawrence <travis.lawrence12@gmail.com>
2023-06-14 08:08:13 -0700

    lets go

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,13 +2,29 @@ const express = require("express");
 const { db } = require("../db");
 const router = express.Router();
 
+router.get("/teams", async function (req, res, next) {
+  const resp = await db.team.findMany();
+  res.json({ teams: resp });
+});
+
 router.get("/team", async function (req, res, next) {
-  const { teamId } = req.query;
-  const team = await db.team.findUnique({ where: { id: +teamId } });
-  team.players = await db.player.findMany({
-    where: { teamId: String(teamId) },
-  });
-  res.json({ team });
+  const { id, name } = req.query;
+  let team;
+  if (id !== undefined) {
+    team = await db.team.findUnique({ where: { id: +id } });
+  } else if (name) {
+    const resp = await db.team.findMany({ where: { name } });
+    team = resp?.[0];
+  }
+
+  if (team?.id !== undefined) {
+    team.players = await db.player.findMany({
+      where: { teamId: String(team.id) },
+    });
+    res.json({ team });
+  } else {
+    res.json({ response: "no team found!" });
+  }
 });
 
 router.get("/players", async function (req, res, next) {
