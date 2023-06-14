cbde708a3eabc99e5278550f43818645388543e0
Travis Lawrence <travis.lawrence12@gmail.com>
2023-06-14 12:31:56 -0700

    first request

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,31 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { ITeam } from "./types";
 
 function App() {
+  const [team, setTeam] = useState<ITeam | undefined>();
+
+  const fetchTeam = async (teamName: string) => {
+    const { data } = await axios(`http://localhost:5005/team?name=${teamName}`);
+    setTeam(data.team);
+  };
+
+  useEffect(() => {
+    fetchTeam("Crocodons");
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>Welcome to Really Epic Hardball</p>
+        <h3>{team?.name}</h3>
+        <ul>
+          {team?.players?.map((player) => {
+            const fullName = `${player.firstName} ${player.lastName}`;
+            return <li key={fullName}>{fullName}</li>;
+          })}
+        </ul>
       </header>
     </div>
   );
---
+++ client/src/types.ts
@@ -0,0 +1,9 @@
+export interface IPlayer {
+  firstName: string;
+  lastName: string;
+}
+
+export interface ITeam {
+  name: string;
+  players: IPlayer[];
+}
