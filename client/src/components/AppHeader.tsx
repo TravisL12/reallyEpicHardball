b968ab9be5fa7fc5c7b611b09d38a1e34bfa2599
Travis Lawrence <travis.lawrence12@gmail.com>
2023-06-15 14:36:35 -0700

    favicon

--- client/src/components/AppHeader.tsx
+++ client/src/components/AppHeader.tsx
@@ -2,8 +2,8 @@ import { SFlex, SHeader, SLink } from "./styles";
 
 const AppHeader = () => {
   return (
-    <SFlex gap="20px">
-      <SHeader>Welcome to Really Epic Hardball</SHeader>
+    <SFlex gap="20px" align="center">
+      <SHeader>Really Epic Hardball</SHeader>
       <SFlex gap="5px">
         <SLink to="/">Players</SLink>
         <SLink to="/teams">Teams</SLink>
